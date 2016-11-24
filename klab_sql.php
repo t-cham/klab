91f8bd4d9c82fdf0c32a817412da8f8d283d40a7
taiju <taiju4451@gmail.com>
2016-11-24 10:28:25 +0900

    形態素解析を組み込んだ

--- klab_sql.php
+++ klab_sql.php
@@ -10,6 +10,9 @@
 	ini_set('display_errors', 1);
 	ini_set("memory_limit","4096M");
 
+	 //アプリケーションIDのセット
+	$id = "dj0zaiZpPUwwcFdlbHl2YWh4VCZzPWNvbnN1bWVyc2VjcmV0Jng9NTE-";
+
 	$dsn = 'mysql:dbname=tweetdata_europe_2016;host=localhost';
 	$user = 'root';
 	$password = 'root';
@@ -31,8 +34,21 @@
 		foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row){
 			echo "<pre>";
 			echo $row['tweet_text'];	
- 			echo "</pre>";	
- 		}
+			echo "</pre>";
+
+			 //形態素解析したい文章
+			$word = $row['tweet_text'];
+    		//URLの組み立て
+			$url = "http://jlp.yahooapis.jp/MAService/V1/parse?appid=" . $id . "&results=ma&sentence=" . urlencode($word);
+    		//戻り値をパース
+			$parse = simplexml_load_file($url);
+    		
+			foreach($parse->ma_result->word_list->word as $value){
+				echo $value->surface;
+				echo "／";
+			}
+		}
+
 
 	}catch (PDOException $e){
 		print('Error:'.$e->getMessage());

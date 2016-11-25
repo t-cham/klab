45dd580d38d8e924399f7b128626ebdd6c6386bb
taiju <taiju4451@gmail.com>
2016-11-25 17:03:07 +0900

    ちょい変更

---
+++ mecab_test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+</head>
+<body>
+	
+    <?php
+
+    $mecab= shell_exec("echo 'Rからmecabを使って形態素解析を行ってみたいと思う' | mecab");
+
+
+    ?>
+
+
+</body>
+</html>
